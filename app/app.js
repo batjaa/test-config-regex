43d9c0790ee46c60d1806af98d737bc33a28da8f
Batjaa Batbold <batjaa0615@gmail.com>
2016-06-03 07:54:16 -0700

    Add test for consuming regex defined in config

--- app/app.js
+++ app/app.js
@@ -7,6 +7,12 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+if (config.myRegex.test) {
+  console.log('No worries, env defined regex is working.');
+} else {
+  console.error('Oh oh... env defined regex is not interpreting right.');
+}
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
